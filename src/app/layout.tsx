2b96f6b71ab9fcdf4c4adce55162be403a70b146
Neaz-mq <neazmorshed666@gmail.com>
2025-04-07 16:21:50 +0600

    working navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,12 +59,18 @@ export default function RootLayout({
           >
            <div className="min-h-screen">
             <Navbar></Navbar>
+            
             <main className="py-8">
-               {/* container to center the content */}
-               <div className="max-w-7xl mx-auto px-4">
-               </div>
-            {children}
-            </main>       
+                {/* container to center the content */}
+                <div className="max-w-7xl mx-auto px-4">
+                  <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
+                    <div className="hidden lg:block lg:col-span-3">
+                      Sidebar 
+                    </div>
+                    <div className="lg:col-span-9">{children}</div>
+                  </div>
+                </div>
+              </main>     
            </div>
           </ThemeProvider>
         </body>

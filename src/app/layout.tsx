6d994500bfb6227c6551048007a5fb9405332c0b
Neaz-mq <neazmorshed666@gmail.com>
2025-04-27 11:51:35 +0600

    updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <head>
           {/* Prevents theme flicker before hydration */}
           <script

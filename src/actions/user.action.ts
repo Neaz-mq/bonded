a382dd01865249d6256ee9ad802f9e8f86494f77
Neaz-mq <neazmorshed666@gmail.com>
2025-04-28 16:52:59 +0600

    createpost working

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -50,4 +50,13 @@ export async function syncUser() {
         },
       });
     }
+    export async function getDbUserId() {
+      const { userId: clerkId } = await auth();
+      if (!clerkId) return null;
     
+      const user = await getUserByClerkId(clerkId);
+    
+      if (!user) throw new Error("User not found");
+    
+      return user.id;
+    }    
